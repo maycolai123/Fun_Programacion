65235c6bdf1f0347d41be39eefee0a86f0a758fc
Maycol <mycolteman18ajadatilla@gmail.com>
2026-08-29 20:34:33 -0500

    estructuras fundamentales

--- Python/0.fundamentos.py
+++ Python/0.fundamentos.py
@@ -1,1 +1,30 @@
-# fundamentos del lenguaje Python
+# Fundamentos del Lenguaje Python
+
+# numero enetero
+edad = 27
+
+# cadena de texto
+nombre = "maycol"
+
+# numero decimal 
+precio = 19.99
+
+# valor logico
+estado = False
+
+# valor caracter
+genero = "M"
+
+# trae datos
+nombre = input("ingrese nombre")
+
+# salida de datos
+print(f"hola, {nombre}")
+
+# comentario de 1 linea
+'''
+comentario bloque
+'''
+
+#ejecutar programa pýthon
+python fundamentos.py 
