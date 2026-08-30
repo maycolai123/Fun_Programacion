3363801c637a4c2d66f76c3636fcf6d2ef6be3c7
Maycol <mycolteman18ajadatilla@gmail.com>
2026-08-29 21:01:25 -0500

    estructuras en C#

---
+++ CSharp/Estructuras_de_control/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructuras_de_control
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("estructurasdecontrol");
+            // secuencial
+            string nombre;
+
+            
+            Console.WriteLine("ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese edad:");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine($"nomnre: {nombre} , Edad : {edad} ");
+
+            // condicional
+            if (edad >= 18)
+            {
+                Console.WriteLine("eres mayor");
+
+            }
+            else { 
+                 Console.WriteLine("eres menor");
+
+            }
+
+            // repetitivas
+            Console.WriteLine("ingresar numero del 1 al 3:");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1: Console.WriteLine("opcion 1"); break;
+                case 2: Console.WriteLine("opcion 2"); break;
+                case 3: Console.WriteLine("opcion 3"); break;
+                default: Console.WriteLine("opcion invalido"); break;
+            }
+
+
+
+            }
+    }
+}
